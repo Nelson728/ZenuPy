d6418ddd1529f01474f4ed2b3f52474e69473045
Nelson728 <devlindehey07@gmail.com>
2026-03-10 21:50:06 -0400

    Abstracted the api

--- index.cjs
+++ index.cjs
@@ -1,13 +1,16 @@
 /*-----------------------------
 Globals
 *///----------------------------
+
 const { Client, GatewayIntentBits, REST, Routes, ActivityType, Events, MessageFlags } = require("discord.js");
 require('dotenv').config();
 const { writeFile, readFile } = require("fs").promises
+const api = require("./api.cjs");
 
 /*-----------------------------
 MySQL
 *///----------------------------
+
 console.log("Starting SQL");
 const mysql = require("mysql2/promise");
 
@@ -20,110 +23,6 @@ const pool = mysql.createPool({
     connectionLimit: 10,
 });
 
-
-/*------------------------------
-Express server for API and such
-*///----------------------------
-console.log("Starting webserver");
-const http = require('http');
-const express = require("express");
-
-const app = express()
-const port = 1727
-app.set('port', port);
-
-const server = http.createServer(app);
-app.use(express.json());
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', () => { console.log("API listening on port " + port) });
-
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-
-    var bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-// CORS
-
-app.use(async (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    if (req.method === "OPTIONS") return res.sendStatus(200);
-    next();
-});
-
-// API endpoints
-
-app.get('/v1/logs/:type/:id', async (req, res) => {
-    //let content = JSON.stringify(req.body.content);
-    let { success, output } = {success: false, output: "Default response"}
-    let type = req.params.type;
-
-    switch (type) {
-        case "ai":
-            ({ success, output } = await handleAI(req));
-            return res.json({ success: success, output });
-        case "command":
-            ({ success, output } = await handleCommands(req));
-            return res.json({ success: success, output });
-
-        default:
-            return res.status(400).json({ error: "Unknown type" });
-    }
-});
-
-/*------------------------------
-API handlers
-*///----------------------------
-
-async function handleAI(req) {
-    let id = req.params.id;
-    let raw = await readFile("./Logs/v1Log.json", "utf8");
-    let log = JSON.parse(raw);
-    let output
-
-    for (let e of log) {
-        if (e.timestamp.toString() == id) {
-            output = e.content.response
-            return { success: true, output: output }
-        }
-    }
-    return { success: false, output: "Could not find log" }
-}
-async function handleCommands(req) {
-    let id = req.params.id;
-    let raw = await readFile("./Logs/v1Log.json", "utf8");
-    let log = JSON.parse(raw);
-    let output
-    for (let e of log) {
-        if (e.timestamp.toString() == id) {
-            output = e
-            return { success: true, output: output }
-        }
-    }
-    return { success: false, output: "Could not find log" }
-}
 
 /*------------------------------
 Discord bot
@@ -185,7 +84,7 @@ client.on(Events.InteractionCreate, async interaction => {
 
     if (interaction.commandName === 'ping') {
         await interaction.reply('Pong!');
-        await CommandLog("ping", interaction.user.id, interaction.guildId);
+        await CommandLog({cmd: "ping", user: interaction.user.id, guild: interaction.guildId})
     }
     if (interaction.commandName === 'prompt') {
         let prompt = interaction.options.getString("prompt");
@@ -214,7 +113,7 @@ client.on(Events.InteractionCreate, async interaction => {
         }
         await interaction.deleteReply();
         await interaction.followUp(output);
-        await CommandLog("prompt", interaction.user.id, interaction.guildId, [prompt], output);
+        await CommandLog({cmd: "prompt", user: interaction.user.id, guild: interaction.guildId, args: [prompt], response: output })
     }
 });
 
@@ -222,7 +121,7 @@ client.on(Events.InteractionCreate, async interaction => {
 Utility Functions
 *///----------------------------
 
-async function CommandLog(cmd, timestamp = Date.now(), user, guild, args = [], response = "No response") {
+async function CommandLog({cmd, timestamp = Date.now(), user, guild, args = [], response = "No response"}) {
     await Log("command", cmd, timestamp, user, guild, args, response);
 }
 async function Log(event, cmd, timestamp, user, guild, args, response) {
@@ -240,7 +139,7 @@ async function Log(event, cmd, timestamp, user, guild, args, response) {
         }
     }
     oLog.push(log);
-    console.log(JSON.stringify(log, null, 2))
+    console.log(JSON.stringify(log, null, 2));
     await writeFile("./Logs/v1Log.json", JSON.stringify(oLog, null, 2));
 
 
