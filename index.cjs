5a97528fd87cb37f5f84205ef6ab958a7a3b0e43
Nelson728 <devlindehey07@gmail.com>
2026-03-29 00:52:24 -0400

    Removed commented out code

--- index.cjs
+++ index.cjs
@@ -134,46 +134,4 @@ client.on(Events.MessageCreate, async message => {
     let args = message.content.slice(1).split(/ +/);
     let command = args.shift().toLowerCase();
 })
-/*------------------------------
-Utility Functions
-*///----------------------------
-
-// async function CommandLog({ cmd, timestamp = Date.now(), user, guild, args = [], response = "No response" }) {
-//     await Log("command", cmd, timestamp, user, guild, args, response);
-// }
-// async function MessageLog({ user, guild, args = "" }) {
-//     await Log({ event: "deleted_message", timestamp: Date.now(), user: user, guild: guild, args: args })
-//     //"deleted_message", null, Date.now(), user, guild, args, null
-// }
-// async function Log({ event, cmd = null, timestamp, user, guild, args, response = null }) {
-//     let raw = await readFile("./Logs/v1Log.json", "utf8");
-//     let oLog = JSON.parse(raw);
-//     let content = {};
-//     if (event == "command") {
-//         content = {
-//             command: cmd,
-//             user: user,
-//             guild: guild,
-//             args: args,
-//             response: response
-//         }
-//     } else if (event == "deleted_message") {
-//         content = {
-//             message: args,
-//             user: user,
-//             guild: guild
-//         }
-//     }
-
-//     let log = {
-//         timestamp: timestamp,
-//         event: event,
-//         content: content
-//     }
-//     oLog.push(log);
-//     console.log(JSON.stringify(log, null, 2));
-//     await writeFile("./Logs/v1Log.json", JSON.stringify(oLog, null, 2));
-
-
-// }
 client.login(process.env.TOKEN);
