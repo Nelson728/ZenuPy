66b328c68a48e6ba98e65d7bdfbf34db45a38bb6
Nelson728 <devlindehey07@gmail.com>
2026-03-30 22:37:11 -0400

    Change
    CORS accepted request from POST to GET

--- Utils/api.cjs
+++ Utils/api.cjs
@@ -51,7 +51,7 @@ function onError(error) {
 
 app.use(async (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     if (req.method === "OPTIONS") return res.sendStatus(200);
     next();
